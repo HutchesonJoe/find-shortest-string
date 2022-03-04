7afa32b6d2e365d9391dbaadb31b05f89d317628
Joe <hutcheson2001@yahoo.com>
2022-03-04 08:54:30 -0500

    Completed assignment

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,6 +1,14 @@
 function findShortestString(arr) {
-  // type your code here
-}
+  let stringLengths = []
+  for (let string of arr){
+    splitStr = string.split('');
+    let splitLength = splitStr.length;
+    stringLengths.push(splitLength);
+  }
+  let minIndex = stringLengths.indexOf(Math.min(...stringLengths))
+    return arr[minIndex]
+} 
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -17,10 +25,30 @@ if (require.main === module) {
   console.log("Expecting: 'lily'");
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
-  // BENCHMARK HERE
+  const startTime = Date.now();
+  let longArr = []
+
+  for (i=100; i<10000; i + 100){
+    longArr.push(i)
+  }
+
+  for (i =0; i < 1000; i++){
+    findShortestString(['flower', 'juniper', 'lily', 'dadelion']);
+    findShortestString(longArr)
+  }
+
+  const avgTime = (Date.now() - startTime)/ 2000
+  console.log(avgTime)
 }
+
+
+
 
 module.exports = findShortestString;
 
-// Please add your pseudocode to this file
+// Given an array of strings, return the shortest string length; if two strings have the same length, return the first one.
+//this function takes an array of strings
+//interates through the array converting EACH string to it's own array
+//find the first minimum array.length
+//rejoins the array into a string and returns
 // And a written explanation of your solution
